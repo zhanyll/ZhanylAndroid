2f5ff167c24899e7fad4fbd949c87ed23a785071
zhanyll <janylayka95@gmail.com>
2022-03-05 15:57:02 +0600

    hw57,58,59

--- app/src/main/java/com/example/zhanylandroid/MainActivity.kt
+++ app/src/main/java/com/example/zhanylandroid/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity(), Clicked {
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container, MainFragment())
+            .addToBackStack()
             .commit()
     }
 
