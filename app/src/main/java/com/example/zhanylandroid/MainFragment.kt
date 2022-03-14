d3bf9e537dfebd9adf6db873bcf7c1258a6ead5e
zhanyll <janylayka95@gmail.com>
2022-03-14 17:57:14 +0600

    hw47 not finished

--- app/src/main/java/com/example/zhanylandroid/MainFragment.kt
+++ app/src/main/java/com/example/zhanylandroid/MainFragment.kt
@@ -1,6 +1,28 @@
 package com.example.zhanylandroid
 
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 
 class MainFragment: Fragment(R.layout.main_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val img = view.findViewById<AppCompatImageView>(R.id.img)
+
+        object : CountDownTimer(1000, 10000) {
+            override fun onTick(p0: Long) {
+                Glide.with(view.context)
+                    .load("https://c.tadst.com/gfx/600x337/winter-lake.jpg?1")
+                    .into(img)
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+    }
 }
